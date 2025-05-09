80687cfdeb821f35179b1bf21adbbc6ca778069a
welson-rodrigues <welson.rosendo@escolar.ifrn.edu.br>
2025-05-09 10:31:46 -0300

    Texto e Input

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TextInput } from "react-native";
 
 export default function Index() {
+  const [text, onChangeText] = useState("Digite sua senha:");
+  //const [number, onChangeNumber] = useState("");
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <Text style={styles.baseText}>Login</Text>
+
+      <TextInput
+          style={styles.input}
+          placeholder= "teste"
+          value={text}
+          onChangeText={onChangeText}
+        />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
