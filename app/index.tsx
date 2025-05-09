b3a11e8ae236f45f8dfb6383df8eef483b63b3d9
welson-rodrigues <welson.rosendo@escolar.ifrn.edu.br>
2025-05-09 11:44:24 -0300

    Colocando imagem

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Button, Image } from "react-native";
 
 export default function Index() {
   const [text, onChangeText] = useState("");
   const [number, onChangeNumber] = useState("");
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Image
+        style={styles.careca}
+        source={require('../assets/images/pao_careca.jpg')}
+      />
       <Text style={styles.baseText}>Login</Text>
 
       <TextInput
@@ -15,7 +19,7 @@ export default function Index() {
           value={text}
           onChangeText={onChangeText}
         />
-
+        
         <Text style={styles.baseText}>Senha</Text>
 
         <TextInput
@@ -33,6 +37,12 @@ export default function Index() {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    //flex: 1,
+    //alignItems: 'center', 
+    //justifyContent: 'center',
+    padding: 16,
+  },
   baseText: {
     fontFamily: 'Cochin',
   },
@@ -41,5 +51,11 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+  },
+  careca: {
+    width: 200,
+    height: 200,
+    alignSelf: 'center',
+    marginBottom: 20,
   },
 });
